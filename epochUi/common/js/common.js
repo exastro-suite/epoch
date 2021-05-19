0af739fe2d63c241d18243e7598ab1b075dc5b50
satoshi shiota <epoch_member@omcs.jp.nec.com>
2021-05-19 09:57:35 +0000

    画面マニフェストパラメータ保存・読込

--- epochUi/common/js/common.js
+++ epochUi/common/js/common.js
@@ -17,12 +17,15 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//   画面の初期表示設定
+//   画面共通
 // 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 function initialScreen() {
 
-    const $header = $('#header'),
+    const $window = $( window ),
+          $body = $('body'),
+          $container =$('#container'),
+          $header = $('#header'),
           $side = $('#side');
 
     const storage = new webStorage();
@@ -58,8 +61,113 @@ function initialScreen() {
             $side.addClass( sideMenuCloseClass );
         }
     });
+    
+    /* -------------------------------------------------- *\
+       title属性ポップアップ
+    \* -------------------------------------------------- */
+    $body.on({
+      'mouseenter': function(){
+        const $target = $( this ),
+              title = $target.attr('title');
+        if ( title !== undefined ) {
+          // デフォルトtitileを無効にするためいったん削除
+          $target.removeAttr('title');
+          
+          // Popup追加
+          const popupClass = ( $target.is('.epoch-popup-m') )? 'epoch-popup-m-block': 'epoch-popup-block';
+          const $popup = $('<div/>', {
+            'class': popupClass,
+            'text': title
+          })
+          $body.append( $popup );
+          
+          let s = 1; // scale
+          
+          // 対象がワークスペースイメージ内の場合scaleを取得
+          const $wsImage = $target.closest('.workspace-setting-image');
+          if ( $wsImage.length && $wsImage.css('transform') !== undefined ) {
+            const matrix = $wsImage.css('transform').replace(/^matrix\((.+)\)/, '$1');
+            s = matrix.split(',')[0];
+          }
+          
+          // 位置
+          const m = 8,
+                wW = $window.width(),
+                tW = $target.outerWidth() * s,
+                tH = $target.outerHeight() * s,
+                tL = $target.offset().left,
+                tT = $target.offset().top,
+                pW = $popup.outerWidth(),
+                pH = $popup.outerHeight();
+
+          let l = ( tL + tW / 2 ) - ( pW / 2 ),
+              t = tT - pH - m;
+          
+          // Windowサイズを超える場合は調整
+          if ( t <= 0 ) t = tT + tH + m;
+          if ( wW < l + pW ) l = wW - pW - m;
+          if ( l <= 0 ) l = m;
+          
+          $popup.css({
+            'width': pW,
+            'height': pH,
+            'left': l,
+            'top': t
+          });
+          
+        }
+      },
+      'mouseleave': function(){
+        const $target = $( this ),
+              $popup = $body.find('.epoch-popup-block, .epoch-popup-m-block'),
+              title = $popup.text();
+        $popup.remove();
+        
+        // titleを戻す
+        $target.attr('title', title );        
+      }
+    }, '.epoch-popup, .epoch-popup-m');
 
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   共通
+// 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+function epochCommon(){}
+epochCommon.prototype = {
+  // テキストの無害化
+  'textEntities': function( text ) {
+    const entities = [
+      ['&', 'amp'],
+      ['\"', 'quot'],
+      ['\'', 'apos'],
+      ['<', 'lt'],
+      ['>', 'gt'],
+    ];
+    if ( text !== undefined ) {
+      for ( var i = 0; i < entities.length; i++ ) {
+        text = text.replace( new RegExp( entities[i][0], 'g'), '&' + entities[i][1] + ';' );
+      }
+    }
+    return text;
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//   
+// 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+(function(){
+  $.fn.yaml = function(){
+    const $text = $( this ),
+          text = $text.text();
+    $text.html( text );
+  };
+})();
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //
