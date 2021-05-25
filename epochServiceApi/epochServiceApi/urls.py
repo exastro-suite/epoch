17657f27dc311167b6036b339a0b41a5af129159
matsumoto-epoch <kazuhiro.matsumoto@ncontr.com>
2021-05-25 01:49:33 +0000

    manifest template urls added, copyrite added

--- epochServiceApi/epochServiceApi/urls.py
+++ epochServiceApi/epochServiceApi/urls.py
@@ -37,5 +37,6 @@ urlpatterns = [
     path('pipeline/', include('pipeline.urls')),
     path('pipelineParameter/', include('pipelineParameter.urls')),
     path('manifestParameter', include('manifestParameter.urls')),
+    path('workspace/<int:workspace_id>/templates', include('manifestTemplate.urls')),
     path('cdExecDesignation/', include('cdExecDesignation.urls')),
 ]
