5aaa22d2c6a7e7e9be0885c2a55819f6c666e8c8
okada yuhei <kazuhiro.matsumoto@ncontr.com>
2021-07-12 04:42:30 +0000

    firstcommit

---
+++ epochRsOrganization/api_organization.py
@@ -0,0 +1,77 @@
+#   Copyright 2019 NEC Corporation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from flask import Flask, request, abort, jsonify
+from datetime import datetime
+import os
+import json
+
+import globals
+import common
+from dbconnector import dbconnector
+from dbconnector import dbcursor
+import da_workspace
+import da_manifest
+
+# 設定ファイル読み込み・globals初期化
+app = Flask(__name__)
+app.config.from_envvar('CONFIG_API_WORKSPACE_PATH')
+globals.init(app)
+
+# workspaceテーブルに確保済みのcolumn
+workspace_allocated_columns=['workspace_id','create_at','update_at']
+
+@app.route('/alive', methods=['GET'])
+def alive():
+    """死活監視
+
+    Returns:
+        Response: HTTP Respose
+    """
+    return jsonify({"result": "200", "time": str(datetime.now(globals.TZ))}), 200
+
+@app.route('/organization', methods=['POST'])
+def create_organization():
+    """オーガナイゼーション登録
+
+    Returns:
+        response: HTTP Respose
+    """
+    globals.logger.debug('CALL create_organization')
+
+    try:
+        # # Requestからspecification項目を生成する
+        # specification = convert_workspace_specification(request.json)
+
+        # with dbconnector() as db, dbcursor(db) as cursor:
+        #     # workspace情報 insert実行(戻り値：追加したワークスペースID)
+        #     workspace_id = da_workspace.insert_workspace(cursor, specification)
+
+        #     globals.logger.debug('insert workspaced_id:{}'.format(str(workspace_id)))
+
+        #     # workspace履歴追加
+        #     da_workspace.insert_history(cursor, workspace_id)
+
+        #     # workspace情報 データ再取得
+        #     fetch_rows = da_workspace.select_workspace_id(cursor, workspace_id)
+
+        # # Response用のjsonに変換
+        # response_rows = fetch_rows
+
+        # globals.logger.info('CREATED workspace:{}'.format(str(workspace_id)))
+
+        # return jsonify({"result": "200", "rows": response_rows })
+
+    except Exception as e:
+        return common.serverError(e)
