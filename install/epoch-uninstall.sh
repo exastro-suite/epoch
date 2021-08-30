6fcd4b6868ee0d56c1097b98aff2c66dd978b719
shiota-2021 <shiota-satoshi@ncontr.com>
2021-08-30 17:24:40 +0900

    uninstaller v0.1.1

--- install/epoch-uninstall.sh
+++ install/epoch-uninstall.sh
@@ -13,8 +13,14 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+#
+# epoch version 0.1.1 uninstaller
+#
+
 NS_NAME_EPOCH=epoch-system
+NS_NAME_WORKSPACE=epoch-workspace
 NS_NAME_PIPELINE=epoch-tekton-pipelines
+NS_NAME_PIPELINE_V011=epoch-tekton-pipelines-1
 
 BASEDIR=`dirname "$0"`
 
@@ -27,6 +33,9 @@ if [ ! -f "${BASEDIR}/epoch-pv.yaml" ]; then
 	exit 1
 fi
 
+#
+# version 0.1.0 tekton pipeline uninstall
+#
 POD_NAME_CICD=`kubectl get pod -n ${NS_NAME_EPOCH} | sed -n -e '/^[e]poch-cicd-api-/s/ .*$//p'`
 
 if [ ! -z "${POD_NAME_CICD}" ]; then
@@ -41,6 +50,9 @@ if [ ! -z "${POD_NAME_CICD}" ]; then
 	kubectl exec -it ${POD_NAME_CICD} -n ${NS_NAME_EPOCH} -- kubectl delete -f /app/epoch/epochCiCdApi/resource/conv/tekton-common --ignore-not-found
 fi
 
+#
+# version 0.1.0 tekton pipeline uninstall
+#
 if [ ! -z "${POD_NAME_CICD}" ]; then
 	echo "-- DELETE TEKTON pipelinerun --"
 	kubectl exec -it ${POD_NAME_CICD} -n ${NS_NAME_EPOCH} -- tkn pipelinerun delete --all -f -n ${NS_NAME_PIPELINE}
@@ -58,17 +70,50 @@ if [ ! -z "${POD_NAME_CICD}" ]; then
 	kubectl exec -it ${POD_NAME_CICD} -n ${NS_NAME_EPOCH} -- tkn task delete --all -f -n ${NS_NAME_PIPELINE}
 fi
 
+#
+# version 0.1.1 tekton pipeline uninstall
+#
+POD_NAME_TEKTON=`kubectl get pod -n ${NS_NAME_EPOCH} | sed -n -e '/^[e]poch-control-tekton-api-/s/ .*$//p'`
+
+if [ ! -z "${POD_NAME_TEKTON}" ]; then
+	echo "-- DELETE TEKTON PIPELINE --"
+	kubectl exec -it ${POD_NAME_TEKTON} -n ${NS_NAME_EPOCH} -- kubectl delete -f /var/epoch/tekton --ignore-not-found
+fi
+
+#
+# version 0.1.1 tekton pipeline uninstall
+#
+if [ ! -z "${POD_NAME_TEKTON}" ]; then
+	echo "-- DELETE TEKTON pipelinerun --"
+	kubectl exec -it ${POD_NAME_TEKTON} -n ${NS_NAME_EPOCH} -- tkn pipelinerun delete --all -f -n ${NS_NAME_PIPELINE_V011}
+	echo "-- DELETE TEKTON taskrun --"
+	kubectl exec -it ${POD_NAME_TEKTON} -n ${NS_NAME_EPOCH} -- tkn taskrun delete --all -f -n ${NS_NAME_PIPELINE_V011}
+	echo "-- DELETE TEKTON eventlistener --"
+	kubectl exec -it ${POD_NAME_TEKTON} -n ${NS_NAME_EPOCH} -- tkn eventlistener delete --all -f -n ${NS_NAME_PIPELINE_V011}
+	echo "-- DELETE TEKTON triggerbinding --"
+	kubectl exec -it ${POD_NAME_TEKTON} -n ${NS_NAME_EPOCH} -- tkn triggerbinding delete --all -f -n ${NS_NAME_PIPELINE_V011}
+	echo "-- DELETE TEKTON triggertemplate --"
+	kubectl exec -it ${POD_NAME_TEKTON} -n ${NS_NAME_EPOCH} -- tkn triggertemplate delete --all -f -n ${NS_NAME_PIPELINE_V011}
+	echo "-- DELETE TEKTON pipeline --"
+	kubectl exec -it ${POD_NAME_TEKTON} -n ${NS_NAME_EPOCH} -- tkn pipeline delete --all -f -n ${NS_NAME_PIPELINE_V011}
+	echo "-- DELETE TEKTON task --"
+	kubectl exec -it ${POD_NAME_TEKTON} -n ${NS_NAME_EPOCH} -- tkn task delete --all -f -n ${NS_NAME_PIPELINE_V011}
+fi
+
 if [ ! -z "${POD_NAME_CICD}" ]; then
 	echo "-- DELETE WORKSPACE PODS --"
-	kubectl exec -it ${POD_NAME_CICD} -n ${NS_NAME_EPOCH} -- kubectl delete -f /app/epoch/epochCiCdApi/resource/ita_install.yaml -n epoch-workspace --ignore-not-found
+	kubectl exec -it ${POD_NAME_CICD} -n ${NS_NAME_EPOCH} -- kubectl delete -f /app/epoch/epochCiCdApi/resource/ita_install.yaml -n ${NS_NAME_WORKSPACE} --ignore-not-found
 	kubectl exec -it ${POD_NAME_CICD} -n ${NS_NAME_EPOCH} -- kubectl delete -f /app/epoch/epochCiCdApi/resource --ignore-not-found
 fi
 
 echo "-- DELETE NAMESPACE epoch-tekton-pipelines --"
-kubectl delete namespace epoch-tekton-pipelines
+# version 0.1.0 tekton pipeline uninstall
+kubectl delete namespace ${NS_NAME_PIPELINE} --ignore-not-found
+# version 0.1.1 tekton pipeline uninstall
+kubectl delete namespace ${NS_NAME_PIPELINE_V011} --ignore-not-found
 
 echo "-- DELETE NAMESPACE epoch-workspace --"
-kubectl delete namespace epoch-workspace
+kubectl delete namespace ${NS_NAME_WORKSPACE} --ignore-not-found
 
 echo "-- DELETE EPOCH POD --"
 kubectl delete -f "${BASEDIR}/epoch-install.yaml" --ignore-not-found
