b20b892ceba9b598c5faefa24077754ff1bdcb8a
Eiji Nakahashi <eiji-nakahashi@ncontr.com>
2021-10-26 10:20:36 +0900

    Include authentication support

--- install/make-epoch-install.sh
+++ install/make-epoch-install.sh
@@ -13,31 +13,54 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-BASEDIR=`dirname $0`
+BASE_DIR=`dirname $0`
 
-ALL_MANIFESTS="${BASEDIR}/epoch-install.yaml"
-SOURCE_MANIFEST="${BASEDIR}/source"
+ALL_MANIFESTS="${BASE_DIR}/epoch-install.yaml"
+SOURCE_MANIFEST="${BASE_DIR}/source"
+TEMPLATES_DIR="${BASE_DIR}/source/templates"
+
+# ---- templatesフォルダ内のtemplateファイルをもとに定義用のyaml生成 ----
+kubectl create cm gateway-conf-template -n exastro-platform-authentication-infra --dry-run=client -o yaml \
+    --from-file=${TEMPLATES_DIR}/epoch-system-template.conf       \
+    --from-file=${TEMPLATES_DIR}/epoch-ws-argocd-template.conf    \
+    --from-file=${TEMPLATES_DIR}/epoch-ws-ita-template.conf       \
+    --from-file=${TEMPLATES_DIR}/epoch-ws-sonarqube-template.conf \
+    >   ${SOURCE_MANIFEST}/gateway-conf-template.yaml
+
 
 # ---- source内のyamlファイル定義 ----
 YAMLFILES=()
 YAMLFILES+=("tekton-pipeline-release.yaml")
 YAMLFILES+=("tekton-trigger-release.yaml")
-YAMLFILES+=("epochSystem.yaml")
-YAMLFILES+=("proxySetting.yaml")
-YAMLFILES+=("epochCiCdApiConfig.yaml")
-YAMLFILES+=("epochCiCdApi.yaml")
-YAMLFILES+=("epochControlInsideGitLabApi.yaml")
-YAMLFILES+=("epochControlTektonApi.yaml")
-YAMLFILES+=("epochRsOrganizationApi.yaml")
-YAMLFILES+=("epochRsWorkspaceApi.yaml")
-YAMLFILES+=("epochRsCiResultApi.yaml")
-YAMLFILES+=("epochServiceApi.yaml")
-YAMLFILES+=("epochUi.yaml")
-YAMLFILES+=("organization_db.yaml")
-YAMLFILES+=("workspace_db.yaml")
-YAMLFILES+=("tekton_pipeline_db.yaml")
-YAMLFILES+=("tekton_pipelinerun_db.yaml")
+YAMLFILES+=("epoch-system.yaml")
+YAMLFILES+=("proxy-setting.yaml")
+YAMLFILES+=("epoch-cicd-api-config.yaml")
+YAMLFILES+=("epoch-cicd-api.yaml")
+YAMLFILES+=("epoch-control-inside-gitlab-api.yaml")
+YAMLFILES+=("epoch-control-tekton-api.yaml")
+YAMLFILES+=("epoch-rs-organization-api.yaml")
+YAMLFILES+=("epoch-rs-workspace-api.yaml")
+YAMLFILES+=("epoch-rs-ci-result-api.yaml")
+YAMLFILES+=("epoch-service-api.yaml")
+YAMLFILES+=("epoch-ui.yaml")
+YAMLFILES+=("organization-db.yaml")
+YAMLFILES+=("workspace-db.yaml")
+YAMLFILES+=("tekton-pipeline-db.yaml")
+YAMLFILES+=("tekton-pipelinerun-db.yaml")
 
+# 認証基盤
+YAMLFILES+=("exastro-platform-authentication-infra.yaml")
+YAMLFILES+=("exastro-platform-authentication-infra-role.yaml")
+YAMLFILES+=("authentication-infra-env.yaml")
+YAMLFILES+=("authentication-infra-secret.yaml")
+YAMLFILES+=("keycloak.yaml")
+YAMLFILES+=("gateway-conf-template.yaml")
+YAMLFILES+=("nodeport-template.yaml")
+YAMLFILES+=("authentication-infra-api.yaml")
+
+YAMLFILES+=("authentication-infra-setting.yaml")
+
+# tekton deloy後に入れる必要があるので最後にする
 YAMLFILES+=("tekton-dashbord-release.yaml")
 YAMLFILES+=("tekton-dashbord-nodeport.yaml")
 YAMLFILES+=("tekton-trigger-interceptors.yaml")
