a559dd7ee6c5014d9aa4f7ecc06e1ed28b8ae5a0
shiota-2021 <shiota-satoshi@ncontr.com>
2021-11-18 15:34:04 +0900

    CALL API

---
+++ epochUi/common/js/workspace_list.js
@@ -0,0 +1,181 @@
+/*
+#   Copyright 2019 NEC Corporation
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+*/
+// JavaScript Document
+
+function workspaceList( list ) {
+    
+    // テーブルヘッダー
+    const tableHeader = [
+      {
+        'title': 'ワークスペース名',
+        'type': 'text',
+        'width': '20%',
+        'align': 'left',
+        'sort': 'on',
+        'filter': 'on',
+      },
+      {
+        'title': 'オーガナイゼーション名',
+        'type': 'list',
+        'width': '20%',
+        'align': 'left',
+        'sort': 'on',
+        'filter': 'on',
+      },
+      {
+        'title': '最終更新日時',
+        'type': 'date',
+        'width': '160px',
+        'align': 'center',
+        'sort': 'on',
+        'filter': 'on',
+      },
+      {
+        'title': '備考',
+        'type': 'text',
+        'width': 'auto',
+        'align': 'left',
+        'sort': 'on',
+        'filter': 'on'
+      },
+      {
+        'type': 'hoverMenu',
+        'menu': {
+          'edit': {
+            'icon': 'icon-edit',
+            'text': 'ワークスペース編集',
+            'separate': 'on'
+          },
+          'delete': {
+            'icon': 'icon-trash',
+            'text': 'ワークスペース削除'
+          }
+        }
+      }
+    ];
+    
+    // テーブルボディ
+    const tableBody = [];
+
+    // Table表示用のデータを作成する
+    const updateTableBodyData = function(){
+      // 配列全削除
+      tableBody.splice(0);
+
+      let i = 0;
+      for ( const key in list ) {
+        tableBody[i++] = [
+          list[key].name,
+          list[key].organization,
+          list[key].lastModified,
+          list[key].note,
+          key
+        ];
+      }
+    };
+    updateTableBodyData();
+    
+    $('.content-header').find('.content-menu-button').on('click', function(){
+      location.href = 'workspace.html';
+    });
+    
+    // Table作成
+    const et = new epochTable(),
+          $table = et.setup('#list', tableHeader, tableBody, {'sortCol': 2, 'sortType': 'desc'} );
+
+    if ( $table ) {
+      
+      // メニュー
+      $table.on('click', '.et-hm-b', function(){
+        const $button = $( this ),
+              type = $button.attr('data-button'),
+              idKey = $button.attr('data-key');
+
+        if ( list[ idKey ] ) {
+          switch( type ) {
+            // 編集
+            case 'edit':
+              location.href = 'workspace.html';
+              break;
+            // 削除
+            case 'delete':
+              if ( confirm('削除しますか？') ) {
+                $button.mouseleave();
+                delete list[ idKey ];
+                updateTableBodyData();
+                et.update( tableBody );
+                }
+              break;
+          }
+        } else {
+          window.console.warn('Specified workspace(ID:' + idKey + ') does not exist.');
+        }
+      });
+    }
+
+}
+
+
+$(function(){
+    // // （ダミーデータ）
+    // const workspaceListDummy = [];
+
+    // /* ランダムデータ作成 */
+    // const fn = new epochCommon();
+    // const number = 1000;
+    // const workspaceName = ['workspace','ワークスペース','EPOCH'],
+    //       organizations = ['〇〇社の開発A','〇〇社の開発B','〇〇社の開発C'];
+
+    // for ( let i = 0; i < number; i++ ) {
+    //     let date = new Date();
+    //     date.setSeconds(date.getSeconds() - i);
+    //     date.setMinutes(date.getMinutes() - i);
+    //     date.setHours(date.getHours() - i);
+
+    //     const workspaceNum =  Math.floor( Math.random() * workspaceName.length ),
+    //           organizationsNum = Math.floor( Math.random() * organizations.length );
+
+
+    //     workspaceListDummy[i] = {
+    //       'name': workspaceName[workspaceNum] + i,
+    //       'note': 'あああああああああああああああああああああああああああああああああああああああああああああああ',
+    //       'organization': organizations[organizationsNum],
+    //       'lastModified': fn.formatDate(date, 'yyyy/MM/dd HH:mm:ss')
+    //     };
+    // }
+
+    // workspaceList( workspaceListDummy );
+
+    console.log("GET /api2/workspace");
+    $.ajax({
+      "type": "GET",
+      "url": window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/api2/workspace"
+    }).done(function(data) {
+      console.log("RESPONSE GET /api2/workspace:");
+      console.log(JSON.stringify(data));
+
+      workspaceListData = [];
+      for(var i=0; i<data.rows.length; ++i) {
+        workspaceListData.push({
+          id: data.rows[i].id,
+          name: data.rows[i].name,
+          note: data.rows[i].remarks,
+          lastModified: data.rows[i].update_at
+        });
+      }
+      workspaceList(workspaceListData);
+    });
+});
