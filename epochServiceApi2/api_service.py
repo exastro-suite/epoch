56ad1bc6d9d8c80858983e7b4d30e5e86dec1e5b
kazuhiro matsumoto <kazuhiro.matsumoto@ncontr.com>
2021-11-26 16:22:29 +0900

    update epoch-service-api2

--- epochServiceApi2/api_service.py
+++ epochServiceApi2/api_service.py
@@ -111,7 +111,7 @@ def call_pod(workspace_id):
         globals.logger.debug('#' * 50)
 
         if request.method == 'POST':
-            # CIパイプライン情報設定
+            # workspace作成
             return post_pod(workspace_id)
         else:
             # エラー
@@ -234,6 +234,10 @@ def get_workspace_list():
     Returns:
         Response: HTTP Respose
     """
+
+    app_name = "ワークスペース情報:"
+    exec_stat = "一覧取得"
+    error_detail = ""
 
     try:
         globals.logger.debug('#' * 50)
@@ -394,6 +398,9 @@ def put_workspace(workspace_id):
 def post_pod(workspace_id):
     """ワークスペース作成
 
+    Args:
+        workspace_id (int): ワークスペースID
+
     Returns:
         Response: HTTP Respose
     """
@@ -407,7 +414,51 @@ def post_pod(workspace_id):
         globals.logger.debug('CALL {}'.format(inspect.currentframe().f_code.co_name))
         globals.logger.debug('#' * 50)
 
-        ret_status = 200
+        # ヘッダ情報
+        post_headers = {
+            'Content-Type': 'application/json',
+        }
+
+        # 引数をJSON形式で受け取りそのまま引数に設定
+        post_data = request.json.copy()
+
+        # workspace post送信
+        api_url = "{}://{}:{}/workspace/{}".format(os.environ['EPOCH_CONTROL_WORKSPACE_PROTOCOL'],
+                                                   os.environ['EPOCH_CONTROL_WORKSPACE_HOST'],
+                                                   os.environ['EPOCH_CONTROL_WORKSPACE_PORT'],
+                                                   workspace_id)
+        response = requests.post(api_url, headers=post_headers, data=json.dumps(post_data))
+        globals.logger.debug("post workspace response:{}".format(response.text))
+        
+        if response.status_code != '200':
+            error_detail = 'workspace post処理に失敗しました'
+            raise Exception
+
+        # argocd post送信
+        api_url = "{}://{}:{}/workspace/{}/argocd".format(os.environ['EPOCH_CONTROL_ARGOCD_PROTOCOL'],
+                                                          os.environ['EPOCH_CONTROL_ARGOCD_HOST'],
+                                                          os.environ['EPOCH_CONTROL_ARGOCD_PORT'],
+                                                          workspace_id)
+        response = requests.post(api_url, headers=post_headers, data=json.dumps(post_data))
+        globals.logger.debug("post argocd response:{}".format(response.text))
+
+        if response.status_code != '200':
+            error_detail = 'argocd post処理に失敗しました'
+            raise Exception
+
+        # ita post送信
+        api_url = "{}://{}:{}/workspace/{}/it-automation".format(os.environ['EPOCH_CONTROL_ITA_PROTOCOL'],
+                                                                 os.environ['EPOCH_CONTROL_ITA_HOST'],
+                                                                 os.environ['EPOCH_CONTROL_ITA_PORT'],
+                                                                 workspace_id)
+        response = requests.post(api_url, headers=post_headers, data=json.dumps(post_data))
+        globals.logger.debug("post it-automation response:{}".format(response.text))
+
+        if response.status_code != '200':
+            error_detail = 'it-automation post処理に失敗しました'
+            raise Exception
+
+        ret_status = response.status_code
 
         # 戻り値をそのまま返却        
         return jsonify({"result": ret_status}), ret_status
@@ -420,6 +471,9 @@ def post_pod(workspace_id):
 
 def post_ci_pipeline(workspace_id):
     """CIパイプライン情報設定
+
+    Args:
+        workspace_id (int): ワークスペースID
 
     Returns:
         Response: HTTP Respose
@@ -434,7 +488,118 @@ def post_ci_pipeline(workspace_id):
         globals.logger.debug('CALL {}'.format(inspect.currentframe().f_code.co_name))
         globals.logger.debug('#' * 50)
 
-        ret_status = 200
+        # ヘッダ情報
+        post_headers = {
+            'Content-Type': 'application/json',
+        }
+
+        # 引数をJSON形式で受け取りそのまま引数に設定
+        post_data = request.json.copy()
+
+        # epoch-control-inside-gitlab-api の呼び先設定
+        api_url_gitlab = "{}://{}:{}/workspace/{}/gitlab".format(os.environ["EPOCH_CONTROL_INSIDE_GITLAB_PROTOCOL"], 
+                                                                 os.environ["EPOCH_CONTROL_INSIDE_GITLAB_HOST"], 
+                                                                 os.environ["EPOCH_CONTROL_INSIDE_GITLAB_PORT"],
+                                                                 workspace_id)
+
+        # epoch-control-github-api の呼び先設定
+        api_url_github = "{}://{}:{}/workspace/{}/github/webhooks".format(os.environ["EPOCH_CONTROL_GITHUB_PROTOCOL"], 
+                                                                          os.environ["EPOCH_CONTROL_GITHUB_HOST"], 
+                                                                          os.environ["EPOCH_CONTROL_GITHUB_PORT"],
+                                                                          workspace_id)
+
+        # epoch-control-tekton-api の呼び先設定
+        api_url_tekton = "{}://{}:{}/listener/{}".format(os.environ["EPOCH_CONTROL_TEKTON_PROTOCOL"], 
+                                                         os.environ["EPOCH_CONTROL_TEKTON_HOST"], 
+                                                         os.environ["EPOCH_CONTROL_TEKTON_PORT"],
+                                                         workspace_id)
+
+        # epoch-control-ita-api の呼び先設定
+        api_url_ita = "{}://{}:{}/workspace/{}/it-automation/settings".format(os.environ['EPOCH_CONTROL_ITA_PROTOCOL'],
+                                                                              os.environ['EPOCH_CONTROL_ITA_HOST'],
+                                                                              os.environ['EPOCH_CONTROL_ITA_PORT'],
+                                                                              workspace_id)
+
+        # パイプライン設定(GitLab Create Project)
+        request_body = json.loads(request.data)
+        git_projects = []
+
+        # アプリケーションコード
+        if request_body['ci_config']['pipelines_common']['git_repositry']['housing'] == 'inner':
+            for pipeline_ap in request_body['ci_config']['pipelines']:
+                ap_data = {
+                    'git_repositry': {
+                        'user': request_body['ci_config']['pipelines_common']['git_repositry']['user'],
+                        'token': request_body['ci_config']['pipelines_common']['git_repositry']['token'],
+                        'url': pipeline_ap['git_repositry']['url'],
+                    }
+                }
+                git_projects.append(ap_data)
+        # IaC
+        for pipeline_iac in request_body['ci_config']['environments']:
+            if pipeline_iac['git_housing'] == 'inner':
+                iac_data = {
+                    'git_repositry': {
+                        'user': pipeline_iac['git_user'],
+                        'token': pipeline_iac['git_token'],
+                        'url': pipeline_iac['git_url'],
+                    }
+                }
+                git_projects.append(iac_data)
+
+        for proj_data in git_projects:
+            # gitlab/repos post送信
+            response = requests.post('{}/repos'.format(api_url_gitlab), headers=post_headers, data=json.dumps(proj_data))
+            globals.logger.debug("post gitlab/repos response:{}".format(response.text))
+
+            if response.status_code != '200':
+                error_detail = 'gitlab/repos post処理に失敗しました'
+                raise Exception
+
+        if request_body['ci_config']['pipelines_common']['git_repositry']['housing'] == 'outer':
+            # github/webhooks post送信
+            response = requests.post( api_url_github, headers=post_headers, data=post_data)
+            globals.logger.debug("post github/webhooks response:{}".format(response.text))
+
+            if response.status_code != '200':
+                error_detail = 'github/webhooks post処理に失敗しました'
+                raise Exception
+        else:
+            # パイプライン設定(GitLab webhooks)
+            for pipeline_ap in request_body['ci_config']['pipelines']:
+                ap_data = {
+                    'git_repositry': {
+                        'user': request_body['ci_config']['pipelines_common']['git_repositry']['user'],
+                        'token': request_body['ci_config']['pipelines_common']['git_repositry']['token'],
+                        'url': pipeline_ap['git_repositry']['url'],
+                    },
+                    'webhooks_url': pipeline_ap['webhooks_url'],
+                }
+
+                response = requests.post('{}/webhooks'.format(api_url_gitlab) + "workspace/1/gitlab/webhooks", headers=post_headers, data=json.dumps(ap_data))
+                globals.logger.debug("post gitlab/webhooks response:{}".format(response.text))
+
+                if response.status_code != '200':
+                    error_detail = 'gitlab/webhooks post処理に失敗しました'
+                    raise Exception
+
+        # listener post送信
+        response = requests.post(api_url_tekton, headers=post_headers, data=json.dumps(post_data))
+        globals.logger.debug("post listener response:{}".format(response.text))
+
+        if response.status_code != '200':
+            error_detail = 'listener post処理に失敗しました'
+            raise Exception
+
+        # it-automation/settings post送信
+        response = requests.post(api_url_ita, headers=post_headers, data=json.dumps(post_data))
+        globals.logger.debug("post it-automation/settings response:{}".format(response.text))
+
+        if response.status_code != '200':
+            error_detail = 'it-automation/settings post処理に失敗しました'
+            raise Exception
+
+        ret_status = response.status_code
 
         # 戻り値をそのまま返却        
         return jsonify({"result": ret_status}), ret_status
@@ -447,6 +612,9 @@ def post_ci_pipeline(workspace_id):
 
 def post_cd_pipeline(workspace_id):
     """CDパイプライン情報設定
+
+    Args:
+        workspace_id (int): ワークスペースID
 
     Returns:
         Response: HTTP Respose
@@ -461,7 +629,80 @@ def post_cd_pipeline(workspace_id):
         globals.logger.debug('CALL {}'.format(inspect.currentframe().f_code.co_name))
         globals.logger.debug('#' * 50)
 
-        ret_status = 200
+        # ヘッダ情報
+        post_headers = {
+            'Content-Type': 'application/json',
+        }
+
+        # 引数をJSON形式で受け取りそのまま引数に設定
+        post_data = request.json.copy()
+
+        # epoch-control-argocd-api の呼び先設定
+        api_url = "{}://{}:{}/workspace/{}/argocd/settings".format(os.environ['EPOCH_CONTROL_ARGOCD_PROTOCOL'],
+                                                                   os.environ['EPOCH_CONTROL_ARGOCD_HOST'],
+                                                                   os.environ['EPOCH_CONTROL_ARGOCD_PORT'],
+                                                                   workspace_id)
+        # argocd/settings post送信
+        response = requests.post(api_url, headers=post_headers, data=json.dumps(post_data))
+        globals.logger.debug("post argocd/settings response:{}".format(response.text))
+
+        if response.status_code != '200':
+            error_detail = 'argocd/settings post処理に失敗しました'
+            raise Exception
+
+        # authentication-infra-api の呼び先設定
+        api_url_epai = "{}://{}:{}/".format(os.environ["EPOCH_EPAI_API_PROTOCOL"], 
+                                            os.environ["EPOCH_EPAI_API_HOST"], 
+                                            os.environ["EPOCH_EPAI_API_PORT"])
+
+        # postする情報
+        clients = [
+            {
+                "client_id" :   'epoch-ws-{}-ita'.format(workspace_id),
+                "client_host" : os.environ["EPOCH_EPAI_HOST"],
+                "client_protocol" : "https",
+                "client_port" : "31183",
+                "conf_template" : "epoch-ws-ita-template.conf",
+                "backend_url" : "http://it-automation.epoch-workspace.svc:8084/",
+            },
+            {
+                "client_id" :   'epoch-ws-{}-argocd'.format(workspace_id),
+                "client_host" : os.environ["EPOCH_EPAI_HOST"],
+                "client_protocol" : "https",
+                "client_port" : "31184",
+                "conf_template" : "epoch-ws-argocd-template.conf",
+                "backend_url" : "https://argocd-server.epoch-workspace.svc/",
+            },
+            {
+                "client_id" :   'epoch-ws-{}-sonarqube'.format(workspace_id),
+                "client_host" : os.environ["EPOCH_EPAI_HOST"],
+                "client_protocol" : "https",
+                "client_port" : "31185",
+                "conf_template" : "epoch-ws-sonarqube-template.conf",
+                "backend_url" : "http://sonarqube.epoch-tekton-pipeline-1.svc:9000/",
+            },
+        ]
+
+        # post送信（アクセス情報生成）
+        # exec_stat = "認証基盤 初期情報設定"
+        # for client in clients:
+        #     response = requests.post("{}{}/{}/{}".format(api_url_epai, 'settings', os.environ["EPOCH_EPAI_REALM_NAME"], 'clients'), headers=post_headers, data=json.dumps(client))
+
+        #     # 正常時以外はExceptionを発行して終了する
+        #     if response.status_code != 200:
+        #         globals.logger.debug(response.text)
+        #         error_detail = "認証基盤 初期情報設定の生成に失敗しました。 {}".format(response.status_code)
+        #         raise Exception
+
+        # exec_stat = "認証基盤 設定読み込み"
+        # response = requests.put("{}{}".format(api_url_epai, 'apply_settings'))
+
+        # # 正常時以外はExceptionを発行して終了する
+        # if response.status_code != 200:
+        #     error_detail = "認証基盤 設定読み込みに失敗しました。 {}".format(response.status_code)
+        #     raise Exception
+
+        ret_status = response.status_code
 
         # 戻り値をそのまま返却        
         return jsonify({"result": ret_status}), ret_status
