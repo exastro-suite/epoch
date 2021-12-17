dea9534b8c879b770b07fcf02f3e56ca7bb5e8ed
kazuhiro matsumoto <kazuhiro.matsumoto@ncontr.com>
2021-12-17 13:22:14 +0900

    update svc2 text_list.py

--- epochServiceApi2/ja/text_list.py
+++ epochServiceApi2/ja/text_list.py
@@ -15,4 +15,25 @@
 
 class TextList:
     lang_array = {}
-    # lang_array['EP020-xxxx'] = ""
+    lang_array['EP000-0001'] = "オーナー"
+    lang_array['EP000-0002'] = "管理者"
+    lang_array['EP000-0003'] = "メンバー管理"
+    lang_array['EP000-0004'] = "CI設定"
+    lang_array['EP000-0005'] = "CI確認"
+    lang_array['EP000-0006'] = "CD設定"
+    lang_array['EP000-0007'] = "CD実行"
+    lang_array['EP000-0008'] = "CD確認"
+    lang_array['EP000-0009'] = "可能な操作"
+    lang_array['EP000-0010'] = "ワークスペース参照"
+    lang_array['EP000-0011'] = "ワークスペース更新（名称）"
+    lang_array['EP000-0012'] = "ワークスペース更新（CI）"
+    lang_array['EP000-0013'] = "ワークスペース更新（CD）"
+    lang_array['EP000-0014'] = "ワークスペース削除"
+    lang_array['EP000-0015'] = "オーナーロール設定"
+    lang_array['EP000-0016'] = "メンバー追加"
+    lang_array['EP000-0017'] = "ロール変更"
+    lang_array['EP000-0018'] = "CIパイプライン結果確認"
+    lang_array['EP000-0019'] = "Manifestテンプレート・パラメータ編集"
+    lang_array['EP000-0020'] = "CD実行"
+    lang_array['EP000-0021'] = "CD実行結果確認"
+
