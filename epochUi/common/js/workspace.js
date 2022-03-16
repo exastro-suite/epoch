2337b582e4cf75632047b71e0446f1783500cae6
shiota-2021 <shiota-satoshi@ncontr.com>
2022-03-16 09:19:06 +0900

    update ui : check change argocd, IaC

--- epochUi/common/js/workspace.js
+++ epochUi/common/js/workspace.js
@@ -2715,7 +2715,12 @@ const wsDataCompare = function(){
         if ( b === undefined ) {
             return [ compareStatus.add, b, a ];
         } else if ( b === null || b === '') {
-            return [ compareStatus.add, b, a ];
+          return [ compareStatus.add, b, a ];
+          // if( a === null || a === '' ) {
+          //   return [ compareStatus.equal, b, a ];
+          // } else {
+          //     return [ compareStatus.add, b, a ];
+          // }
         } else if ( a === b ) {
             return [ compareStatus.equal, b, a ];
         } else {
@@ -3002,12 +3007,14 @@ const compareInfo = function( modalID, compareData ){
           wsDataJSON['environment'][item]['text']                         = data_environments[i]['name'];
           wsDataJSON['environment'][item][item + '-environment-name']     = data_environments[i]['name'];
           wsDataJSON['environment'][item][item + '-environment-deploy-select'] = data_environments[i]['deploy_destination']['cluster_kind'];
-          wsDataJSON['environment'][item][item + '-environment-url']      = data_environments[i]['deploy_destination']['cluster_url'];
+          //wsDataJSON['environment'][item][item + '-environment-url']      = data_environments[i]['deploy_destination']['cluster_url'];
           wsDataJSON['environment'][item][item + '-environment-namespace']= data_environments[i]['deploy_destination']['namespace'];
           if(data_environments[i]['deploy_destination']['cluster_kind'] == "external") {
+            wsDataJSON['environment'][item][item + '-environment-url']      = data_environments[i]['deploy_destination']['cluster_url'];
             wsDataJSON['environment'][item][item + '-environment-authentication-token']= data_environments[i]['deploy_destination']['authentication_token'];
             wsDataJSON['environment'][item][item + '-environment-certificate']= data_environments[i]['deploy_destination']['base64_encoded_certificate'];
           } else {
+            wsDataJSON['environment'][item][item + '-environment-url']      = null;
             wsDataJSON['environment'][item][item + '-environment-authentication-token']= null;
             wsDataJSON['environment'][item][item + '-environment-certificate']= null;
           }
@@ -3033,8 +3040,8 @@ const compareInfo = function( modalID, compareData ){
         // data_workspaceからwsDataJSONのマニフェストgitリポジトリ情報へ書き出す
         wsDataJSON['git-service-argo'] = {
           'git-service-argo-account-select' : data_workspace['cd_config']['environments_common']['git_repositry']['account_select'],
-          'git-service-argo-user' : data_workspace['cd_config']['environments_common']['git_repositry']['user'],
-          'git-service-argo-token' : data_workspace['cd_config']['environments_common']['git_repositry']['token'],
+          'git-service-argo-user' : data_workspace['cd_config']['environments_common']['git_repositry']['account_select'] == "applicationCode"? null : data_workspace['cd_config']['environments_common']['git_repositry']['user'],
+          'git-service-argo-token' : data_workspace['cd_config']['environments_common']['git_repositry']['account_select'] == "applicationCode"? null :data_workspace['cd_config']['environments_common']['git_repositry']['token'],
           'git-service-argo-select' : data_workspace['cd_config']['environments_common']['git_repositry']['housing'] == 'inner'? 'epoch': data_workspace['cd_config']['environments_common']['git_repositry']['interface'],
         };
 
