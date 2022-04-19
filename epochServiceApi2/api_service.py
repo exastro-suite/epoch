75bafc2e4ea484fb6601a7a4f260464246b23732
kouki-ta <kouki-tanimoto@ncontr.com>
2022-04-19 13:24:42 +0900

    log修正

--- epochServiceApi2/api_service.py
+++ epochServiceApi2/api_service.py
@@ -60,7 +60,7 @@ def call_workspace():
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}: method[{}]'.format(inspect.currentframe().f_code.co_name, request.method))
+        globals.logger.info('Create or GET workspace information. method={}'.format(request.method))
 
         if request.method == 'POST':
             # ワークスペース情報作成へリダイレクト
@@ -84,8 +84,8 @@ def call_workspace_by_id(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
-
+        globals.logger.info('Get or update or partially update workspace information. method={}, workspace_id={}'.format(request.method, workspace_id))
+                    
         if request.method == 'GET':
             # ワークスペース情報取得 Workspace info. get
             return api_service_workspace.get_workspace(workspace_id)
@@ -111,9 +111,7 @@ def call_pod(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.debug('#' * 50)
-        globals.logger.debug('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
-        globals.logger.debug('#' * 50)
+        globals.logger.debug('Create workspace. method={}, workspace_id={}'.format(request.method, workspace_id))
 
         if request.method == 'POST':
             # workspace作成
@@ -137,7 +135,7 @@ def call_ci_pipeline(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
+        globals.logger.info('Set CI pipeline information. method={}, workspace_id={}'.format(request.method, workspace_id))
 
         if request.method == 'POST':
             # CIパイプライン情報設定
@@ -161,7 +159,7 @@ def call_git_commits(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
+        globals.logger.info('Get CI commit list in code repository. method={}, workspace_id={}'.format(request.method, workspace_id))
 
         if request.method == 'GET':
             return api_service_ci.get_git_commits(workspace_id)
@@ -182,7 +180,7 @@ def call_git_hooks(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
+        globals.logger.info('Get webhook execution history. method={}, workspace_id={}'.format( request.method, workspace_id))
 
         if request.method == 'GET':
             return api_service_ci.get_git_hooks(workspace_id)
@@ -203,7 +201,7 @@ def call_registry(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
+        globals.logger.info('Get container registry information. method={}, workspace_id={}'.format(request.method, workspace_id))
 
         if request.method == 'GET':
             # Get container registry information - コンテナレジストリ情報取得
@@ -225,7 +223,7 @@ def call_ci_result(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
+        globals.logger.info('Get CI pipeline results. method={}, workspace_id={}'.format(request.method, workspace_id))
 
         if request.method == 'GET':
             # CIパイプライン結果取得
@@ -248,7 +246,7 @@ def call_ci_result_logs(workspace_id, taskrun_name):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}] taskrun_name[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id, taskrun_name))
+        globals.logger.info('Get CI pipeline results. method={}, workspace_id={}, taskrun_name={}'.format(request.method, workspace_id, taskrun_name))
 
         if request.method == 'GET':
             # CIパイプライン結果取得
@@ -585,9 +583,7 @@ def call_members():
         Response: HTTP Respose
     """
     try:
-        globals.logger.debug('#' * 50)
-        globals.logger.debug('CALL {}:from[{}]'.format(inspect.currentframe().f_code.co_name, request.method))
-        globals.logger.debug('#' * 50)
+        globals.logger.info('Get all users. method={}'.format(request.method))
 
         if request.method == 'GET':
             # all users get
@@ -632,8 +628,8 @@ def call_workspace_member(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
-
+        globals.logger.info('Set or get workspace members. method={}, workspace_id={}'.format(request.method, workspace_id))
+        
         if request.method == 'GET':
             # all workspace members get
             return api_service_member.get_workspace_members(workspace_id)
@@ -658,7 +654,7 @@ def call_workspace_member_cdexec(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
+        globals.logger.info('Get information about CD execution members. method={}, workspace_id={}'.format(request.method, workspace_id))
 
         if request.method == 'GET':
             # cdexec members get
@@ -681,7 +677,7 @@ def call_workspace_leave(workspace_id):
         Response: HTTP Respose
     """
     try:
-        globals.logger.info('CALL {}:from[{}] workspace_id[{}]'.format(inspect.currentframe().f_code.co_name, request.method, workspace_id))
+        globals.logger.info('Exit from a member of the workspace. method={}, workspace_id={}'.format(request.method, workspace_id))
 
 
         return api_service_member.leave_workspace(workspace_id)
